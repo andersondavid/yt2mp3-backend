0330c52d61119b1a5a990d34c904fd9943a90c6f
Anderson David <andersondavidfreitas@gmail.com>
2023-04-08 18:17:16 -0300

    add lint and prettier

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 export const UserController = {
-  getUsers(req: Request, res: Response) {
-    res.send('hello mam')
-  }
+	getUsers(req: Request, res: Response) {
+		res.send('hello mam');
+	},
 };
