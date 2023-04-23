1ed69120b5f3e620e371e9083fc85bc8909a65dc
Anderson David <andersondavidfreitas@gmail.com>
2023-04-23 09:20:52 -0300

    Remove config.ts and add process.env.PORT

--- src/configs/index.ts
+++
@@ -1,7 +0,0 @@
-const configs = {
-	app: {
-		port: 3000
-	}
-}
-
-export default configs
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import configs from './configs/';
+const port = process.env.PORT || 3000;
 
 import app from './app';
 import socket from './services/socket';
@@ -8,8 +8,8 @@ import socket from './services/socket';
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: { origin: '*' } });
 
-httpServer.listen(configs.app.port, () => {
-	console.log('Server listening on port 3000!');
+httpServer.listen(port, () => {
+	console.log(`Server running at ${port}`);
 });
 
 socket(io);
