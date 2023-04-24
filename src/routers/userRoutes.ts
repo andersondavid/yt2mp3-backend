f64a238fe456f633ea2f55fe6953bc095baa2bc1
Anderson David <andersondavidfreitas@gmail.com>
2023-04-23 21:58:51 -0300

    add docker

--- src/routers/userRoutes.ts
+++ src/routers/userRoutes.ts
@@ -4,5 +4,8 @@ import { UserController } from '../controllers/UserController';
 const router = Router();
 
 router.get('/io/', UserController.socket);
+router.get('/hello/', (req, res) => {
+	res.send('hello world')
+});
 
 export default router;
